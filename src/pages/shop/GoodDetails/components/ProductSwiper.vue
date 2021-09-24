e5aaf9a2f440d9549251b49b6117787b218356d7
HuMiao <humiao2006@gmail.com>
2021-09-24 09:16:57 +0800

    商品详情

---
+++ src/pages/shop/GoodDetails/components/ProductSwiper.vue
@@ -0,0 +1,40 @@
+<template>
+  <u-swiper :list="imgUrls" height="750" mode="number" indicator-pos="bottomRight" :circular="true"
+            @click="previewImage"></u-swiper>
+</template>
+
+<script>
+export default {
+  name: "ProductSwiper",
+  props: {
+    imgUrls: {
+      type: Array,
+      default: () => []
+    }
+  },
+  data() {
+    return {
+    }
+  },
+  methods: {
+    previewImage(index) {
+      uni.previewImage({
+        current:index,
+        urls:  this.imgUrls
+        // longPressActions: {
+        //   itemList: ['发送给朋友', '保存图片', '收藏'],
+        //   success: function(data) {
+        //     console.log('选中了第' + (data.tapIndex + 1) + '个按钮,第' + (data.index + 1) + '张图片');
+        //   },
+        //   fail: function(err) {
+        //     console.log(err.errMsg);
+        //   }
+        // }
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
--- src/pages/shop/GoodDetails/index.vue
+++ src/pages/shop/GoodDetails/index.vue
@@ -1,11 +1,31 @@
 <template>
   <view>
-    GoodDetails
+    <ProductSwiper :imgUrls="storeInfo.sliderImageArr"></ProductSwiper>
+    <view style="background-color: white;">
+      
+    </view>
   </view>
 </template>
 <script>
+
+import ProductSwiper from "./components/ProductSwiper";
+
 export default {
-  name: 'GoodDetails'
+  name: 'GoodDetails',
+  components: {ProductSwiper},
+  data() {
+    return {
+      storeInfo: {
+        sliderImageArr: [
+          "https://xjyp.oss-cn-guangzhou.aliyuncs.com/xjyp-wx/20210829/d40386dcd02c449f912f2e942f40aaed.jpg",
+          "https://xjyp.oss-cn-guangzhou.aliyuncs.com/xjyp-wx/20210829/0094a4901855484c9bc8b071d9e33e98.png",
+          "https://xjyp.oss-cn-guangzhou.aliyuncs.com/xjyp-wx/20210829/c50d0669c37b4f92bd1b272209961a25.jpg",
+          "https://xjyp.oss-cn-guangzhou.aliyuncs.com/xjyp-wx/20210829/3f9676f984734734b5ca423eef201bbb.jpg",
+        ]
+      }
+    }
+  }
+
 }
 </script>
 <style lang="scss">
